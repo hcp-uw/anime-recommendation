56296dec8a3a5a5d38779425502ac1233d658cc3
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2025-03-02 12:18:55 -0800

    Updated routing

--- frontend/js/App.tsx
+++ frontend/js/App.tsx
@@ -2,8 +2,9 @@ import * as Sentry from "@sentry/react";
 import cookie from "cookie";
 
 import { OpenAPI } from "./api";
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Layout from "./Layout";
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/Home";
 
@@ -17,12 +18,15 @@ OpenAPI.interceptors.request.use((request) => {
 
 const App = () => (
   <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
-    <Navbar />
-    <Routes >
-      <Route path="/" element={<Home />} />
-      <Route path="/recs" element={<Recommendations />} />
-      <Route path="/about" element={<AboutUs />} />
-    </Routes >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="recs" element={<Recommendations />} />
+          <Route path="about" element={<AboutUs />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </Sentry.ErrorBoundary>
 );
 
