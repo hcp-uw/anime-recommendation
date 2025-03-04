689b5fe66b78891e69bf08ad3e68c393d58f7dfe
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2025-03-04 15:59:46 -0800

    Spacing

--- frontend/js/components/HomeInput/HomeInput.tsx
+++ frontend/js/components/HomeInput/HomeInput.tsx
@@ -1,11 +1,12 @@
 import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav'
+import { Row } from "react-bootstrap";
 
 function UsernameInput() {
     return (
         <Form>
-            <Form.Group className="mx-auto text-center" style={{width: "80vw"}}>
+            <Form.Group className="mx-auto mt-3 text-center" style={{width: "80vw"}}>
                 <Form.Label >Enter MyAnimeList username(s)</Form.Label>
                 <Form.Control type="text" style={{fontSize: "20px", height: "3em"}}/>
             </Form.Group>
@@ -15,19 +16,17 @@ function UsernameInput() {
 
 function GenerateButton() {
     return (
-        <Nav.Link href="/recs" className="mx-auto text-center">
-            <Button variant="button-c" size="lg" className="btn-custom mx-auto px-5" style={{color: "#FDFDFD"}}>
-                Generate Recommendations
-            </Button>
-        </Nav.Link>
+        <Button variant="button-c" size="lg" className="btn-custom mx-auto px-5" style={{color: "#FDFDFD"}}>
+            <Nav.Link href="/recs">Generate Recommendations</Nav.Link>
+        </Button>
     );
 }
 
 export default function HomeInput() {
     return (
-        <div className="d-grid gap-2">
-            <UsernameInput />
-            <GenerateButton />
+        <div className="d-grid gap-4">
+            <Row><UsernameInput /></Row>
+            <Row className="mx-auto text-center"><GenerateButton /></Row>
         </div>  
     );
 }
--- frontend/js/pages/Home.tsx
+++ frontend/js/pages/Home.tsx
@@ -1,16 +1,17 @@
 // import { useState, useEffect } from "react";
 
 // import { RestService } from "../api";
+import { Container } from "react-bootstrap";
 import Brand from "../components/HomeInput/Brand";
 import HomeInput from "../components/HomeInput/HomeInput";
 
 const Home = () => {
 
   return (
-    <>
+    <Container className="py-5">
       <Brand />
       <HomeInput />
-    </>
+    </Container>
   );
 };
 
