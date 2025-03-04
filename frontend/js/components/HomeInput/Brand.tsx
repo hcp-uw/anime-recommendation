be2ded53d49df8a6576b9f2e1d5ecd12926109d9
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2025-03-04 13:57:23 -0800

    Linting

--- frontend/js/components/HomeInput/Brand.tsx
+++ frontend/js/components/HomeInput/Brand.tsx
@@ -1,17 +1,18 @@
 import { Col, Image, Row } from "react-bootstrap";
+
 import KabutoImageSource from "../../../assets/images/kabuto.png";
 
 // Change bug image later
 export default function Brand() {
-    return (
-        <Row className="justify-content-md-center">
-            <Col md="auto" xs >
-                <Image src={KabutoImageSource} />
-            </Col>
-            <Col md="auto" xs >
-                <h1>Kabuto</h1>
-                <h1>Recs</h1>
-            </Col>
-        </Row>
-    );
+  return (
+    <Row className="justify-content-md-center">
+      <Col md="auto" xs>
+        <Image src={KabutoImageSource} />
+      </Col>
+      <Col md="auto" xs>
+        <h1>Kabuto</h1>
+        <h1>Recs</h1>
+      </Col>
+    </Row>
+  );
 }
